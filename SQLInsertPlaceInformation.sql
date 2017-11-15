eed61d26b640a2c738cd52bc6057ca7cfc4736ea
백기훈 <masinogns@gmail.com>
2017-11-15 16:45:25 +0900

    sqldum 20171114

--- SQLInsertPlaceInformation.sql
+++ SQLInsertPlaceInformation.sql
@@ -2,3 +2,5 @@ INSERT INTO (name, phone_number, address)
 VALUES
 (),
 ();
+
+mysqldump -h 183.111.183.101 -u captanp696 -p captanp696 > 20171115_backup.sql
--- src/test/java/MySQL/geocodingTest.java
+++ src/test/java/MySQL/geocodingTest.java
@@ -21,16 +21,17 @@ public class geocodingTest {
 
 //        String path = "제주특별자치도 제주시 거로중길 28"; 주소를 써도되고 명칭을 써도 된다
         //String keyword = "오설록티뮤지엄";
-        String keyword = "해비치컨트리클럽제주";
-        String[] data = geocoding.run(keyword);
-        System.out.println(data[0]+" "+data[1]+" "+data[2]);
+//        String keyword = "해비치컨트리클럽제주";
+
 
         ArrayList<String> ret = readTextFile.read(path);
 
         for (String word  : ret){
-
+            String[] data = geocoding.run(word);
+            System.out.println(data[0]+" "+data[1]+" "+data[2]);
+            dataset.put(word, data);
         }
-        dataset.put(keyword, data);
+
 
 //
 //        ForGeo forGeo = new ForGeo();
